37879d1374cfb4f7a8d4f54c7d2eccfb256e1302
JOÃO CARLOS FORMENTO <joao_c_formento@estudante.sesisenai.org.br>
2025-02-20 16:12:37 -0300

    Jogo finalizado

--- primeiro-jogo/script.js
+++ primeiro-jogo/script.js
@@ -1,26 +1,57 @@
 const canvas = document.getElementById("jogoCanvas");
 const ctx = canvas.getContext('2d');
+let gravidade = 0.5
+let perdeu = false
+let pontos = 0;
 
+document.addEventListener("keypress", (e) =>{
+    if (e.code == "Space"){
+        if(personagem.pulando == false && perdeu == false){
+            personagem.velocidadey = -15;
+            personagem.pulando = true;
+        }
+    }
+})
 
+document.addEventListener("click", (e) => {
+    if(perdeu == true){
+        location.reload()
+    }
+})
 
 function desenhaPersonagem(){
     ctx.fillStyle = 'black';
     ctx.fillRect(personagem.posx, personagem.posy, personagem.tamx, personagem.tamy);
 }
 
+
 const personagem = {
     posx: 50,
     posy: canvas.height - 50,
     tamx: 50,
     tamy: 50,
+    velocidadey: 0,
+    pulando:false,
+    parado:true,
 }
-
 const obstaculo = {
     posx: canvas.width - 100,
     posy: canvas.height - 100,
     tamx: 50,
     tamy: 100,
-    velocidade: 3,
+    velocidade: 5,
+}
+
+function atualizaPersonagem() {
+    if(personagem.pulando == true){
+        personagem.velocidadey += gravidade
+        personagem.posy += personagem.velocidadey
+        
+        if(personagem.posy >= canvas.height - 50 && personagem.pulando == true){
+            personagem.velocidadey = 0;
+            personagem.pulando = false;
+        }   
+    }
 }
 
 function desenhaObstaculo(){
@@ -29,22 +60,29 @@ function desenhaObstaculo(){
 }
 
 function atualizaObstaculo(){
-    obstaculo.posx -= obstaculo.velocidade
+    if(obstaculo.posx + obstaculo.tamx < 0){
+        obstaculo.posx = canvas.width
+        pontos += (obstaculo.velocidade * 10)
+        document.getElementById('pontos').innerHTML = `Pontuação: ${pontos.toFixed()}`
+        obstaculo.velocidade *= 1.1
+    }else{
+        obstaculo.posx -= obstaculo.velocidade
+    }
 }
 
 function verificaColisao(){
     if(
         personagem.posx <= obstaculo.posx + obstaculo.tamx &&
-        personagem.posx >= obstaculo.posx - obstaculo.tamx
+        personagem.posx >= obstaculo.posx - obstaculo.tamx &&
+        personagem.posy <= obstaculo.posy + obstaculo.tamy &&
+        personagem.posy >= obstaculo.posy - obstaculo.tamy
     ){
         gameOver();
     }
 }
 
 function gameOver(){
-    document.addEventListener("click", (e) => {
-        location.reload()
-    })
+    perdeu = true;
     obstaculo.velocidade = 0;
     atualizaObstaculo();
     ctx.fillStyle = 'red';
@@ -57,6 +95,7 @@ function gameOver(){
 function loop () {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     desenhaPersonagem();
+    atualizaPersonagem();
     desenhaObstaculo();
     atualizaObstaculo();
     verificaColisao();
